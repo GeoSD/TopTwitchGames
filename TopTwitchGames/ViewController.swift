9029bc4f2b8f699084abb700f01e330a131af086
Дягилев Георгий Сергеевич <g.djagilev@cft.tu>
2020-05-28 11:37:28 +0700

    Initial Commit

---
+++ TopTwitchGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopTwitchGames
+//
+//  Created by Дягилев Георгий Сергеевич on 28.05.2020.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
